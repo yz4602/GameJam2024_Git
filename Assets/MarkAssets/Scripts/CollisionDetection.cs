5d7e5943a6f741418b5eaddb272e09cb3ef07624
Mark_Z <yz4602@tc.columbia.edu>
2024-01-25 16:36:29 -0500

    Block and bounce logic

--- Assets/MarkAssets/Scripts/CollisionDetection.cs
+++ Assets/MarkAssets/Scripts/CollisionDetection.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
-	private float[] hpAndQg = {-10f, -10f};
+	public PlayerController player;
+	private float[] hpAndQg = {-30f, 0f};
+	private Vector2 contactPosition;
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
@@ -13,26 +15,46 @@ public class CollisionDetection : MonoBehaviour
 		foreach (ContactPoint2D contact in collision.contacts)
 		{
 			Collider2D collider = contact.collider;
-			GameObject collidedObject = collider.gameObject;
-			
+			GameObject collidedObject = collider.gameObject;			
 			// Now you have the specific GameObject that was part of the collision
 			if(collidedObject.layer == LayerMask.NameToLayer("Body"))
 			{
-				if(collision.gameObject.name == "PlayerB")
+				contactPosition = contact.point;
+				PlayerController attackedPlayer = collision.gameObject.GetComponent<PlayerController>();
+				if(!attackedPlayer.GetInvulnerability())
 				{
-					EventCenter.Instance.EventTrigger("PlayerBGetDamage", hpAndQg);
-				}
-				else if(collision.gameObject.name == "PlayerA")
-				{
-					EventCenter.Instance.EventTrigger("PlayerAGetDamage", hpAndQg);
-				}
+					if(!CheckDefense(attackedPlayer))
+					{
+						EventCenter.Instance.EventTrigger(collision.gameObject.name + "GetDamage", hpAndQg);
+						attackedPlayer.SetInvulnerable(true);
+						attackedPlayer.BeVulnerableDelay();
+					}
+				}	
 			}
 		}
 	}
-
-	private void OnCollisionExit2D(Collision2D collision)
-
+	
+	private bool CheckDefense(PlayerController attackedPlayer)
 	{
-		
+		if(attackedPlayer.isDefend)
+		{
+			if(attackedPlayer.defendTime <= 0.2f)
+			{
+				player.GetBounced(transform.position, contactPosition);
+				hpAndQg[0] = 0;
+				hpAndQg[1] = 50;
+				EventCenter.Instance.EventTrigger(player.gameObject.name + "GetDamage", hpAndQg);
+				Debug.Log(player.name + " 被弹反");
+			}
+			else
+			{
+				hpAndQg[0] /= 2;
+				hpAndQg[1] = 15;
+				EventCenter.Instance.EventTrigger(attackedPlayer.gameObject.name + "GetDamage", hpAndQg);
+				Debug.Log(attackedPlayer.name + " 防御");
+			}
+			return true; //Attack be blocked
+		}
+		return false; //Attack not be blocked
 	}
 }
--- Assets/MarkAssets/Scripts/PlayerController.cs
+++ Assets/MarkAssets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,40 +8,71 @@ public class PlayerController : MonoBehaviour
 	public float TorqueForce = 120f;
 	public float StandardAngularVelocity = 180f;
 	private float TorqueToAdd = 0;
-	public bool isAttacked;
+	private bool canController = true;
+	[Tooltip("The time to recover from bounce and attack")]
+	public float recoverTime = 0.3f; // The time to recover from bounce and attack
+	public GameObject defenseShied;
 	public bool isDefend;
 	public float defendTime;
 	private Rigidbody2D rig;
+	private bool isDazed;
+	private bool isInvulnerable;
+
+	
+	private KeyCode clockwiseKey, anticlockwiseKey, defendKey;
 	
 	// Start is called before the first frame update
-	void Start()
+	void Awake()
 	{
 		rig = GetComponent<Rigidbody2D>();
+		if(gameObject.name == "PlayerA")
+		{
+			clockwiseKey = KeyCode.A;
+			anticlockwiseKey = KeyCode.D;
+			defendKey = KeyCode.S;
+		}
+		else if(gameObject.name == "PlayerB")
+		{
+			clockwiseKey = KeyCode.LeftArrow;
+			anticlockwiseKey = KeyCode.RightArrow;
+			defendKey = KeyCode.DownArrow;
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		TorqueToAdd = 0;
 		//Debug.Log(rig.angularVelocity);
-		if(Input.GetKey(KeyCode.A))
+		TorqueToAdd = 0;
+		canController = !isDefend && !isDazed;
+		
+		//Debug.Log(rig.angularVelocity);
+		if(canController)
 		{
-			TorqueToAdd = TorqueForce;
-		}
-		if(Input.GetKey(KeyCode.D))
-		{
-			TorqueToAdd = -TorqueForce;
+			if(Input.GetKey(clockwiseKey))
+			{
+				TorqueToAdd = TorqueForce;
+			}
+			if(Input.GetKey(anticlockwiseKey))
+			{
+				TorqueToAdd = -TorqueForce;
+			}
 		}
 		
-		if(Input.GetKey(KeyCode.S))
+		if(Input.GetKey(defendKey))
 		{
 			isDefend = true;
+			defenseShied.SetActive(true);
 			defendTime += Time.deltaTime;
 		}
 		else
 		{
-			isDefend = false;
-			defendTime = 0;
+			if(isDefend)
+			{
+				isDefend = false;
+				defendTime = 0;
+				defenseShied.SetActive(false);
+			} 
 		}
 	}
 	
@@ -50,4 +82,39 @@ public class PlayerController : MonoBehaviour
 		//Constrain the angular velocity of the weapon's rotation
 		rig.angularDrag = Mathf.Abs(rig.angularVelocity)/StandardAngularVelocity;
 	}
+	
+	public void GetBounced(Vector2 HeadPosition, Vector2 contactPosition)
+	{
+		//TODO:调整弹反的力
+		rig.AddForceAtPosition((HeadPosition - contactPosition) * Math.Max(Mathf.Abs(rig.angularVelocity), 50f) * 15f , contactPosition);
+		isDazed = true;
+		Invoke("RecoverFromDaze", recoverTime);
+	}
+	
+	public void RecoverFromDaze()
+	{
+		isDazed = false;
+	}
+	
+	#region vulnerability setting
+	public void BeVulnerableDelay()
+	{
+		Invoke("GetVulnerable", recoverTime);
+	}
+	
+	private void GetVulnerable()
+	{
+		SetInvulnerable(false);
+	}
+	
+	public void SetInvulnerable(bool b)
+	{
+		isInvulnerable = b;
+	}
+	
+	public bool GetInvulnerability()
+	{
+		return isInvulnerable;
+	}
+	#endregion
 }
