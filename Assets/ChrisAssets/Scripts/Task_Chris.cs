2aab6bca4ca045ce408a41994e502d497764f6d1
XCRS <144189295+CZ2771@users.noreply.github.com>
2024-01-26 15:42:26 -0500

    -End panel inception

---
+++ Assets/ChrisAssets/Scripts/Task_Chris.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task_Chris : MonoBehaviour
+{
+    void Start()
+    {
+        EventCenter.Instance.AddEventListener("PlayerADie", TP1Die);
+        EventCenter.Instance.AddEventListener("PlayerBDie", TP2Die);
+    }
+
+    private void TP1Die(object obj)
+    {
+        Debug.Log("掉qg： " + (obj as float[])[0]);
+        Debug.Log("Monster Die Task");
+
+        UIManager.Instance.ShowPanel<PlayerADiePanel>("PlayerADiePanel");
+        UIManager.Instance.ShowPanel<PlayerBDiePanel>("PlayerBDiePanel");
+    }
+
+    private void TP2Die(object obj)
+    {
+        Debug.Log("掉qg： " + (obj as float[])[0]);
+        Debug.Log("Monster Die Task");
+    }
+
+    private void OnDestroy()
+    {
+        EventCenter.Instance.RemoveEventListener("PlayerADie", TP1Die);
+        EventCenter.Instance.RemoveEventListener("PlayerBDie", TP2Die);
+    }
+}
